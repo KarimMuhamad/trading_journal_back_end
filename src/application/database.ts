10217ad43e8d44a5c0baad556c8e58e55d15837a
Choyim <karimmuhammad551@gmail.com>
2025-11-29 19:14:27 +0700

    test(auth): testing for soft-delete prisma extension fix(database): fixing prisma extension not called

--- src/application/database.ts
+++ src/application/database.ts
@@ -7,7 +7,7 @@ const adapter = new PrismaPg({
     connectionString: process.env.DATABASE_URL as string,
 });
 
-export const prisma = new PrismaClient({
+const prismaBase = new PrismaClient({
     log: [
         { level: 'query', emit: 'event' },
         { level: 'info', emit: 'event' },
@@ -17,21 +17,21 @@ export const prisma = new PrismaClient({
     adapter
 });
 
-prisma.$on('warn', (e) => {
+prismaBase.$on('warn', (e) => {
     logger.warn('Prisma Warning', {
         message: e.message,
         target: e.target,
     });
 });
 
-prisma.$on('error', (e) => {
+prismaBase.$on('error', (e) => {
     logger.error('Prisma Error', {
         message: e.message,
         target: e.target,
     })
 });
 
-prisma.$on('info', (e) => {
+prismaBase.$on('info', (e) => {
     logger.info('Prisma Info', {
         message: e.message
     })
@@ -39,7 +39,7 @@ prisma.$on('info', (e) => {
 
 
 if (process.env.NODE_ENV !== 'production') {
-    prisma.$on('query', (e) => {
+    prismaBase.$on('query', (e) => {
         logger.debug('Prisma Query', {
             query: e.query,
             params: e.params,
@@ -48,6 +48,5 @@ if (process.env.NODE_ENV !== 'production') {
     });
 }
 
-prisma.$extends(softDeleteExtension);
-
+export const prisma = prismaBase.$extends(softDeleteExtension);
 export default prisma;
