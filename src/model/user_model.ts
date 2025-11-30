7f83a041cca960ffd19ce3c5308e3fbb21036730
Choyim <karimmuhammad551@gmail.com>
2025-12-01 04:40:19 +0700

    feat(user): add getUserProfile service implementation and Model/DTO for User - add helper function for user response

--- src/model/user_model.ts
+++ src/model/user_model.ts
@@ -1,1 +1,15 @@
-// TODO Implement user model
+import {User} from "../../prisma/generated/client";
+
+export type UserResponse = {
+  id: string;
+  username: string;
+  email: string;
+};
+
+export function toUserResponse (user: User): UserResponse {
+    return {
+        id: user.id,
+        username: user.username,
+        email: user.email
+    };
+}
--- src/service/user_service.ts
+++ src/service/user_service.ts
@@ -1,3 +1,11 @@
+import {User} from "../../prisma/generated/client";
+import prisma from "../application/database";
+import {toUserResponse, UserResponse} from "../model/user_model";
+
 export class UserService {
-    // TODO Implement UserService
+    static async getUserProfile(user: User) : Promise<UserResponse> {
+        const getUser = await prisma.user.findUnique({where: {id: user.id}});
+
+        return toUserResponse(getUser!);
+    }
 }
