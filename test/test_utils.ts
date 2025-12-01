b4ebdbdd0bd0f8bed0848a4edc9cb12f2aa20167
Choyim <karimmuhammad551@gmail.com>
2025-12-01 20:24:22 +0700

    test(user): testing for endpoint /users/me to get user profile - 200 OK Get User Profile - 401 Unauthorized or Invalid Token

--- test/test_utils.ts
+++ test/test_utils.ts
@@ -6,7 +6,7 @@ import {buildUrl} from "./routes";
 export class AuthTestUtils {
 
     static async deleteAll () {
-        await prisma.user.deleteMany({});
+        await prisma.user.deleteMany();
     }
 
     static async createUser (username: string, email: string, password: string) {
