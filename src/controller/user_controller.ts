787d18c7d7cc38007561eb042180c2cae2ea1ffb
Choyim <karimmuhammad551@gmail.com>
2025-12-01 19:18:35 +0700

    feat(user): add get user profile controller

--- src/controller/user_controller.ts
+++ src/controller/user_controller.ts
@@ -1,3 +1,29 @@
+import logger from "../application/logger";
+import { UserService } from "../service/user_service";
+import { AuthUserRequest } from "../types/auth_type";
+import { NextFunction, Response } from "express";
+import {UserResponse} from "../model/user_model";
+
 export class UserController {
-    // TODO Implement UserController
+    static async getUserProfile(req: AuthUserRequest, res: Response, next: NextFunction) {
+        try {
+            const response: UserResponse = await UserService.getUserProfile(req.user!);
+            res.status(200).json({
+                status: "success",
+                message: "User profile fetched successfully",
+                data: response
+            });
+
+            logger.info("Get User Profile success", {
+                userId: req.user!.id,
+            });
+
+        } catch (e: any) {
+            logger.warn("Get User Profile failed", {
+                message: e.message,
+                status: e.status,
+            });
+            next(e);
+        }
+    }
 }
