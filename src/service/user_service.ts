39072fc2e6d0725f49d87b2987703b432ee4f4ed
Choyim <karimmuhammad551@gmail.com>
2025-12-02 09:34:47 +0700

    WIP: feat: add soft deleting account logic and revoke session

--- src/service/user_service.ts
+++ src/service/user_service.ts
@@ -1,9 +1,10 @@
 import {User} from "../../prisma/generated/client";
 import prisma from "../application/database";
-import {toUserResponse, UserResponse, UserUpdateUsernameRequest} from "../model/user_model";
+import {DeleteAccountRequest, toUserResponse, UserResponse, UserUpdateUsernameRequest} from "../model/user_model";
 import {ErrorResponse} from "../error/error_response";
 import {Validation} from "../validation/validation";
 import {UserValidation} from "../validation/user_validation";
+import argon2 from "argon2";
 
 export class UserService {
     static async getUserProfile(user: User) : Promise<UserResponse> {
@@ -25,5 +26,24 @@ export class UserService {
 
         const updateUser = await prisma.user.update({where: {id: user.id}, data: {username: validateReq.username}});
         return toUserResponse(updateUser!);
+    }
+
+    static async deleteAccount(user: User, req: DeleteAccountRequest) : Promise<void> {
+        const isPasswordValid = await argon2.verify(user.password, req.password);
+        if(!isPasswordValid) throw new ErrorResponse(403, "Invalid password");
+
+        await prisma.$transaction(async (tx) => {
+            await tx.auth_session.updateMany({
+                where: {user_id: user.id},
+                data: {revoked_at: new Date()}
+            });
+
+            await tx.user.update({
+                where: {id: user.id},
+                data: {deleted_at: new Date(), deleted_expires_at: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30)}
+            });
+        });
+
+        // TODO send email to user to notify account deletion
     }
 }
