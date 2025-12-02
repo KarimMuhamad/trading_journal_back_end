0005af656e5d57b9f813ddfdb84f29cffadd773b
Choyim <karimmuhammad551@gmail.com>
2025-12-02 16:29:35 +0700

    feat: add cotroller implementation for delete account ENDPOINT

--- src/controller/user_controller.ts
+++ src/controller/user_controller.ts
@@ -2,7 +2,7 @@ import logger from "../application/logger";
 import { UserService } from "../service/user_service";
 import { AuthUserRequest } from "../types/auth_type";
 import { NextFunction, Response } from "express";
-import {UserResponse, UserUpdateUsernameRequest} from "../model/user_model";
+import {DeleteAccountRequest, UserResponse, UserUpdateUsernameRequest} from "../model/user_model";
 
 export class UserController {
     static async getUserProfile(req: AuthUserRequest, res: Response, next: NextFunction) {
@@ -48,6 +48,29 @@ export class UserController {
                 status: e.status,
             });
 
+            next(e);
+        }
+    }
+
+    static async deleteAccount(req: AuthUserRequest, res: Response, next: NextFunction) {
+        try {
+            const request: DeleteAccountRequest = {
+                password: req.body.password
+            }
+
+            await UserService.deleteAccount(req.user!, request);
+            res.status(200).json({
+                status: "success",
+                message: "Account deleted successfully"
+            });
+            logger.info("Delete Account success", {
+                userId: req.user!.id,
+            });
+        } catch (e: any) {
+            logger.warn("Delete Account failed", {
+                message: e.message,
+                status: e.status,
+            });
             next(e);
         }
     }
