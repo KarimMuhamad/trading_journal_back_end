1737e862ae4378136288feeeac39e5d9edfb8ed3
Choyim <48382277+KarimMuhamad@users.noreply.github.com>
2025-12-12 16:48:48 +0700

    Refactor/enhanced consistency response (#14)
    * refactor: adding file error code
    
    * refactor: add field code and update middleware error
    
    * refactor: updated auth service, and middleware error response
    
    * refactor: updated user service

--- src/error/error_response.ts
+++ src/error/error_response.ts
@@ -1,5 +1,7 @@
+import { ErrorCode } from "./error-code";
+
 export class ErrorResponse extends Error {
-    constructor(public statusCode: number, public message: string) {
+    constructor(public statusCode: number, public message: string, public code?: ErrorCode) {
         super(message);
     }
 }
--- src/middleware/auth_middleware.ts
+++ src/middleware/auth_middleware.ts
@@ -3,16 +3,17 @@ import {Response, NextFunction} from "express";
 import {ErrorResponse} from "../error/error_response";
 import jwt from "jsonwebtoken";
 import prisma from "../application/database";
+import { ErrorCode } from "../error/error-code";
 
 export const authMiddleware = async (req: AuthUserRequest, res: Response, next: NextFunction)=> {
     const token = req.headers.authorization?.split(' ')[1] as string;
 
-    if (!token) throw new ErrorResponse(401, "Unauthorized, no token provided");
+    if (!token) throw new ErrorResponse(401, "Unauthorized, no token provided", ErrorCode.AUTH_UNAUTHORIZED);
 
     const decoded = jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET!) as JWTDecoded;
     const user = await prisma.user.findUnique({where: {id: decoded.id}});
 
-    if (!user) throw new ErrorResponse(401, "Unauthorized, invalid token");
+    if (!user) throw new ErrorResponse(401, "Unauthorized, invalid token", ErrorCode.AUTH_UNAUTHORIZED);
 
     req.user = user;
 
